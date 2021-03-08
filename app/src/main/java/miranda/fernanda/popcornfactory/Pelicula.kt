02671974461d808719e58ae63633b963baeed563
FernandaM27 <fermirandavega@gmail.com>
2021-03-08 00:20:00 -0700

    Video

---
+++ app/src/main/java/miranda/fernanda/popcornfactory/Pelicula.kt
@@ -0,0 +1,8 @@
+package miranda.fernanda.popcornfactory
+
+data class Pelicula ( var titulo: String,
+                      var image: Int,
+                      var header: Int,
+                      var sinopsis: String){
+
+}
